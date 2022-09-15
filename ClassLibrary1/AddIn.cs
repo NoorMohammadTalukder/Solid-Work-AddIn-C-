2b6a03396c7bcef1b00336935309db341d651d6b
YourName <noor11809027@gmail.com>
2022-09-15 13:32:44 +0600

    cylinder addtion done

--- ClassLibrary1/AddIn.cs
+++ ClassLibrary1/AddIn.cs
@@ -1,5 +1,7 @@
 ﻿using CodeStack.SwEx.AddIn;
 using CodeStack.SwEx.AddIn.Attributes;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,8 +36,46 @@ namespace Primitives
                     break;
                 case Commands_e.CreateCylinder:
                     //Todo:  Create a Cylinder
+                    CreateCylinder(0.01,0.01);
                     break;
             }
+        }
+
+        private void CreateCylinder (double diam, double height)
+        {
+            var sketch = CreateSketch(diam / 2);
+            
+            (sketch as IFeature).Select2(false, 0);
+
+            App.IActiveDoc2.FeatureManager.FeatureExtrusion3(true, false, false,
+                (int)swEndConditions_e.swEndCondBlind, (int)swEndConditions_e.swEndCondBlind, 
+                height, 0, false, false, false, false, 0, 0, 
+                false, false, false, false, false, false, false, 
+                (int)swStartConditions_e.swStartSketchPlane, 0, false);
+        }
+
+        private ISketch CreateSketch( double radius)
+        {
+         
+
+
+            var model = App.IActiveDoc2;
+
+            var skMgr = model.SketchManager;
+
+            skMgr.InsertSketch(true);
+            skMgr.AddToDB = true;
+
+            var sketch = skMgr.ActiveSketch;
+
+            skMgr.CreateCircleByRadius(0, 0, 0, radius);
+
+            skMgr.AddToDB = false;
+            skMgr.InsertSketch(true);
+
+            return sketch;
+
+
         }
     }
 }
